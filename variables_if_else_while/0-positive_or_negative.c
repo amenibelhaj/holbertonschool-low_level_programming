5178c8d081f52e01b1d41e76c85e17d6870dcdf5
MAC <mac@MACs-MacBook-Pro.local>
2024-10-22 14:34:08 +0200

    task0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,3 +12,22 @@ int main(void)
 	/* your code goes there */
 	return (0);
 }
+ 
+int main() {
+    int number;
+
+    // Prompt the user for input
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Check the number and print the appropriate message
+    if (number > 0) {
+        printf("%d is positive\n", number);
+    } else if (number == 0) {
+        printf("%d is zero\n", number);
+    } else {
+        printf("%d is negative\n", number);
+    }
+
+    return 0;
+}
