42bde35f87e107ef331063f357aeab333cd56240
amenibelhaj <blameni073@gmail.com>
2024-10-31 13:47:46 +0200

    task6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,30 @@
 #include "main.h"
-
 /**
-* draw a straight line - function that draws a straight line in the terminal.
-*/
-
-void print_line(int n);
+ * print_line - Draws a straight line using underscores.
+ * @n: The number of underscores to print.
+ *
+ * Description: This function prints a horizontal line made of
+ * underscores ('_'). The length of the line is determined by
+ * the parameter `n`. If `n` is greater than 0, it will print
+ * `n` underscores in a row, followed by a newline character.
+ * If `n` is 0 or less, it simply prints a newline.
+ *
+ * Return: void
+ */
+void print_line(int n)
 {
-    
-	while (n > 0)
-	{
-		_putchar('_');
-		n--;
-	}
-	_putchar('\n');
+int i;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
 }
