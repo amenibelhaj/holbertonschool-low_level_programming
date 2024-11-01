f60f8e8d251f0e87531ff465cb2bfd5cc19d9dec
amenibelhaj <blameni073@gmail.com>
2024-11-01 13:31:26 +0200

    task9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+/**
+ * main - Entry point of the FizzBuzz program.
+ *
+ * Description: Prints the numbers from 1 to 100, replacing multiples of
+ * three with "Fizz," multiples of five with "Buzz," and multiples of
+ * both three and five with "FizzBuzz."
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 1 ; i <= 100 ; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i < 100)
+{
+printf(" ");
+}
+}
+printf("\n");
+return (0);
+}
