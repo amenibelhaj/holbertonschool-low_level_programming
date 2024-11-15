da10beed6f94cc5127c10d18b6760bfdbaa62747
amenibelhaj <blameni073@gmail.com>
2024-11-15 13:49:29 +0200

    task3

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,3 +5,5 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
+int *array_range(int min, int max);
