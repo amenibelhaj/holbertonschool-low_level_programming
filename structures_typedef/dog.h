6e4cfedecb8e9a562a97e716f11517c1ef1d89d3
amenibelhaj <blameni073@gmail.com>
2024-11-15 15:00:26 +0200

    doggo

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,15 @@
-#include <stdio.h>
-
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Structure to store information about a dog
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
- */
-struct dog {
-char * name;
+ **/
+struct dog
+{
+char *name;
 float age;
-char * owner;
-};
+char *owner;
+}
