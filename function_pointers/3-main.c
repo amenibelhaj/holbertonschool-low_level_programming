c65ef338add17a8f3c15f7ff2d61304e64f7525b
amenibelhaj <blameni073@gmail.com>
2024-11-19 14:19:23 +0200

    tqsk 1

---
+++ function_pointers/3-main.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point for the calculator program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, or an error code
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*operation)(int, int);
+
+    /* Check argument count */
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+/* Get the operation function */
+ operation = get_op_func(argv[2]);
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+ /* Handle division or modulo by 0 */
+if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+
+    /* Perform the operation */
+result = operation(num1, num2);
+printf("%d\n", result);
+
+return (0);
+}
