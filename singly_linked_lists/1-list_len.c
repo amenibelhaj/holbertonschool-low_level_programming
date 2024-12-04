c28c73bb79991fca78c3b8d50d779e3c2a7815db
amenibelhaj <blameni073@gmail.com>
2024-12-04 14:44:34 +0200

    Returns the number of elements in a linked list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,7 +4,8 @@
  * list_len - Returns the number of elements in a linked list
  * @h: A pointer to the head of the linked list
  *
- * This function traverses the linked list starting from the head node and counts 
+ * This function traverses the linked list starting from the head node
+ * and counts
  * the number of nodes in the list. It returns the total number of nodes.
  *
  * Return: The number of nodes in the linked list
@@ -12,7 +13,7 @@
 
 size_t list_len(const list_t *h)
 {
-size_t len= 0;
+size_t len = 0;
 while (h != NULL)
 {
 len++;
