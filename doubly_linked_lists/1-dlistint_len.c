6a65411c80598d6b939f678916da72018c3b5dde
amenibelhaj <blameni073@gmail.com>
2024-12-05 17:07:37 +0200

    update

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
- * @head: A pointer to the pointer of the first node in the list.
- * @n: The integer to be stored in the new node.
+ * dlistint_len - Returns the number of elements in a doubly linked list
+ * @h: A pointer to the head of the doubly linked list
  *
- * Return: The address of the new node, or NULL if it failed.
+ * Return: The number of nodes in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 
@@ -17,5 +16,5 @@ while (current != NULL)
 count++;
 current = current->next;
 }
-return count;
+return (count);
 }
