de79d2b0df87d7850e822dac633a6bc1ac7485bd
amenibelhaj <blameni073@gmail.com>
2024-12-05 12:47:19 +0200

    Freeing a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the first node in the list.
+ */
+
+ void free_list(list_t *head)
+{
+list_t *current_node;
+list_t *next_node;
+
+current_node = head;
+while (current_node != NULL)
+{
+next_node = current_node->next;
+free(current_node->str);
+free(current_node);
+current_node = next_node;
+}
+}
