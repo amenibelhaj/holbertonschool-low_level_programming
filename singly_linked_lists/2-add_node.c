e4ef962f77d663f6493f9b238ff77c0eca03de49
amenibelhaj <blameni073@gmail.com>
2024-12-05 11:56:59 +0200

    Adds a new node at the beginning of a list_t list updated

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,10 +7,10 @@
  *
  * Return: The address of the new node, or NULL if it failed.
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
+unsigned int len = 0;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
@@ -20,7 +20,11 @@ if (new_node->str == NULL)
 free(new_node);
 return (NULL);
 }
-new_node->len = strlen(str);
+while (str[len] != '\0')
+{
+len++;
+}
+new_node->len = len;
 new_node->next = *head;
 *head = new_node;
 
