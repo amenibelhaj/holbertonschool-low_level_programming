367e7b9bb031d07d6919983179a06a0d898063b8
amenibelhaj <blameni073@gmail.com>
2024-12-06 14:57:15 +0200

    Returning the sum of all the data (n) in a doubly linked list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * sum_dlistint - Returns the sum of all the data (n) in a doubly linked list.
  * @head: A pointer to the head of the doubly linked list.
- * Return: The sum of all the data in the list. If the list is empty, returns 0.
+ * Return: The sum of all the data in the list. If the list is empty,returns 0.
  */
 int sum_dlistint(dlistint_t *head)
 {
