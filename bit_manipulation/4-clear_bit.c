dc10ac78e43705f688f630d364da2407d0ec5600
amenibelhaj <blameni073@gmail.com>
2024-12-07 18:25:00 +0200

    Setting the value of a bit to 0 at a given index

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(*n) * 8)
 return (-1);
-*n &= ~(1UL << index);  // Use AND and NOT to clear the bit at the given index
+*n &= ~(1UL << index);
 return (1);
 }
 
